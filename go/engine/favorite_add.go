8c7f657982ed1e65e5e3e4df331921a845d6f91e
Frederick Akalin <akalin@keyba.se>
2018-08-29 13:39:13 -0700

    Fix more

--- go/engine/favorite_add.go
+++ go/engine/favorite_add.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/keybase/client/go/kbname"
 	"github.com/keybase/client/go/libkb"
 	keybase1 "github.com/keybase/client/go/protocol/keybase1"
 )
@@ -101,7 +102,7 @@ func (e *FavoriteAdd) checkInviteNeeded(m libkb.MetaContext) error {
 	}
 
 	for _, user := range strings.Split(e.arg.Folder.Name, ",") {
-		assertion, ok := libkb.NormalizeSocialAssertion(m.G().MakeAssertionContext(), user)
+		assertion, ok := kbname.NormalizeSocialAssertion(m.G().MakeAssertionContext(), user)
 		if !ok {
 			m.CDebugf("not a social assertion: %s", user)
 			continue
